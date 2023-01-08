bd642ddebd811f67f5e506675f9b41acc5a0320e
Mersed12 <mersed.keco@stud.fh-campuswien.ac.at>
2023-01-08 21:13:39 +0100

    Comitting started console program

---
+++ src/Converter.java
@@ -0,0 +1,26 @@
+public class Converter {
+
+    double amount;
+    String currency;
+
+    public Converter(double amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        double amount;
+
+        double convertedAmount;
+        String firstCurrency;
+        String secondCurrency;
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Welcome");
+        System.out.println("Here is the list of available currencies that can be converted: ");
+        System.out.println(ConversionRate.currencyName());
+        System.out.println("Euro (EUR)\n" + "US Dollar (USD)\n" + "Australian Dollar (AUD)\n");
+        System.out.println("Choose first currency");
+        firstCurrency = sc.next();
+        System.out.println("How much bro?");
+        amount = sc.nextDouble();
+        System.out.println("Choose second currency");
+        secondCurrency = sc.next();
+
+        convertedAmount = Double.parseDouble(ConversionRate.ConversionRate(String.valueOf(amount), firstCurrency, secondCurrency));
+        System.out.println(convertedAmount);
+
     }
 }
+
+    
